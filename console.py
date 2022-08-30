6d33e31a7da00f08a4573f92f9430a5c63e02257
ben <benjamineruvieru@gmail.com>
2022-08-30 22:47:30 +0100

    cmd

--- console.py
+++ console.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
-""""""
+"""Module containing the entry point of the command interpreter."""
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    pass
+    """This class represents the command interpreter
+    of this project."""
+
+    prompt = '(hbnb) '
+
+    def do_help(self, arg):
+        """To get help on a command, type help <topic>.
+        """
+        return super().do_help(arg)
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """Quit command to exit the program"""
+        print("")
+        return True
 
 
 if __name__ == '__main__':
