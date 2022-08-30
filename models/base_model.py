85a77c3b5fc1eda5124305a64a308797b83e894c
ben <benjamineruvieru@gmail.com>
2022-08-30 11:24:34 +0100

    fix errors

--- models/base_model.py
+++ models/base_model.py
@@ -2,40 +2,56 @@
 """
 The Base Module for AirBnB Console
 """
+from unittest import result
 import uuid
-import datetime from datetime
+from datetime import datetime
 
 
 class BaseModel:
-  """The Base class"""
-  
-  def __init__(self):
-    """
-    instatiates an object with it's
-    attributes
-    """
-    self.id = str(uuid.uuid4())
-    self.created_at = datetime.now() 
-    self.updated_at = datetime.now()
-    
-  def __str__(self):
-    """string representation"""
-    return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
-  def save(self):
-    """Updates the public instance attribute updated_at
-    with the current datetime
-    """
-    self.updated_at = datetime.now()
-  
-  def todict(self):
-    """Returns a dictionary containing all keys/values of __dict__
-    of the instance
-    """
-    for key, value in self.__dict__.items():
-      if isinstance(value, datetime):
-        result[key] = value.isoformat()
-      else:
-        result[key] = value
-    result['__class__'] = self.__class__.__name__
-    return result 
+    """The Base class"""
+
+    def __init__(self):
+        """
+        instatiates an object with it's
+        attributes
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """string representation"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the public instance attribute updated_at
+        with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__
+        of the instance
+        """
+        result = {}
+        for key, value in self.__dict__.items():
+            if isinstance(value, datetime):
+                result[key] = value.isoformat()
+            else:
+                result[key] = value
+        result['__class__'] = self.__class__.__name__
+        return result
+
+
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key,
+          type(my_model_json[key]), my_model_json[key]))
