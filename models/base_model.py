2f4933da4cef3bf33f8432465788614899fce64b
ben <benjamineruvieru@gmail.com>
2022-08-30 10:54:33 +0100

    added some doc

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-"""The Base Class Module for AirBnB Console"""
+"""
+The Base Module for AirBnB Console
+"""
 import uuid
 import datetime from datetime
 
 
 class BaseModel:
-  """nitializing the constructor"""
+  """The Base class"""
   
   def __init__(self):
+    """
+    instatiates an object with it's
+    attributes
+    """
     self.id = str(uuid.uuid4())
     self.created_at = datetime.now() 
     self.updated_at = datetime.now()
@@ -17,9 +23,15 @@ class BaseModel:
     return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
     
   def save(self):
+    """Updates the public instance attribute updated_at
+    with the current datetime
+    """
     self.updated_at = datetime.now()
   
   def todict(self):
+    """Returns a dictionary containing all keys/values of __dict__
+    of the instance
+    """
     for key, value in self.__dict__.items():
       if isinstance(value, datetime):
         result[key] = value.isoformat()
