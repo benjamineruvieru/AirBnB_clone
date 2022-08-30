a09e93ec1db513a8688ff9bd1b80027ebf651873
ben <benjamineruvieru@gmail.com>
2022-08-30 11:43:05 +0100

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -10,14 +10,22 @@ from datetime import datetime
 class BaseModel:
     """The Base class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         instatiates an object with it's
         attributes
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """string representation"""
@@ -41,17 +49,3 @@ class BaseModel:
                 result[key] = value
         result['__class__'] = self.__class__.__name__
         return result
-
-
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key,
-          type(my_model_json[key]), my_model_json[key]))
