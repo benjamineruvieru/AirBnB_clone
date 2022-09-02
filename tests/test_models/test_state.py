81d8f311f4e6d6b5f31fe1dc44f197f9b3e0da30
St-Pardon <okc4pardon@gmail.com>
2022-09-02 15:59:05 +0000

    started unit test

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Unit test module for the state model."""
+import unittest
+from models.base_model import BaseModel
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+    """Test class for the State class."""
+    pass
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Unit test module for the user model."""
+import unittest
+from models.base_model import BaseModel
+from models.user import User
+
+
+class TestUser(unittest.TestCase):
+    """Test class for the User class."""
+    pass
