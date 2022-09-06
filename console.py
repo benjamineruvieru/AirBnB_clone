e0dbdda4013c964211abab1de2632237d4811384
St-Pardon <okc4pardon@gmail.com>
2022-09-06 21:06:39 +0000

    advanced attemptedd

--- console.py
+++ console.py
@@ -9,6 +9,10 @@ from models.city import City
 from models.review import Review
 from models.amenity import Amenity
 from models.place import Place
+import re
+import shlex
+import sys
+
 
 avaliable_classes = {'BaseModel': BaseModel, 'User': User,
                      'Amenity': Amenity, 'City': City, 'State': State,
@@ -20,6 +24,108 @@ class HBNBCommand(cmd.Cmd):
     of this project."""
 
     prompt = '(hbnb) '
+
+    def precmd(self, line):
+        """Runs some actions before a line of command is executed.
+        Args:
+            line (str): The line of command to be transformed.
+        Returns:
+            str: The next line of command to execute.
+        """
+        patterns = (
+            r'(?P<class>[a-zA-Z]+)',
+            r'(?P<command>[a-zA-Z]+)',
+            r'(?P<args_txt>.*)',
+        )
+        cls_fxn_fmt = r'{}\s*\.\s*{}\s*\({}\)'.format(
+            patterns[0], patterns[1], patterns[2]
+        )
+        cls_fxn_match = re.fullmatch(cls_fxn_fmt, line)
+        if cls_fxn_match is not None:
+            class_name = cls_fxn_match.group('class')
+            command_name = cls_fxn_match.group('command')
+            args_txt = cls_fxn_match.group('args_txt').strip()
+            args = None
+            cmd_line_parts = []
+            try:
+                args = self.split_func_args(args_txt)
+                cmd_line_parts.append(command_name)
+                cmd_line_parts.append(class_name)
+                for arg in args:
+                    cmd_line_parts.append('"{}"'.format(arg))
+                return ' '.join(cmd_line_parts)
+            except Exception:
+                return line
+        else:
+            return line
+
+    def postcmd(self, stop, line):
+        """Runs some actions after a line of command is executed.
+        Args:
+            stop (bool): The continuation condition.
+            line (str): The line of command that was executed.
+        Returns:
+            bool: The continuation condition.
+        """
+        if not sys.__stdin__.isatty():
+            print('(hbnb) ', end='')
+        return stop
+
+    def split_func_args(self, args_txt):
+        """Splits a function argument section into its arguments.
+        Args:
+            args_txt (str): The function argument section.
+        Returns:
+            list: The list of arguments.
+        """
+        txt = args_txt.strip()
+        quote = None
+        brace = None
+        brace_d = 0
+        a = 0
+        char_p = None
+        pushed_a = False
+        parts = []
+        n = len(txt)
+        for i in range(n):
+            if txt[i] == ',':
+                if (quote is None) and (brace is None):
+                    if not pushed_a:
+                        parts.append(txt[a:i])
+                    else:
+                        pushed_a = False
+                    a = i + 1
+            elif (txt[i] == '{') and (quote is None):
+                if brace is None:
+                    brace = '{'
+                    a = i
+                brace_d += 1
+            elif (txt[i] == '}') and (quote is None):
+                if brace_d > 0:
+                    brace_d -= 1
+                if brace_d == 0:
+                    parts.append(txt[a:i+1])
+                    pushed_a = True
+                    brace = None
+                    brace_d = 0
+                    a = i + 1
+            elif (txt[i] == '"') and (brace is None):
+                if (quote is None):
+                    quote = '"'
+                    a = i + 1
+                else:
+                    parts.append(txt[a:i])
+                    pushed_a = True
+                    quote = None
+                    a = i + 1
+            elif i == n - 1:
+                if not pushed_a:
+                    parts.append(txt[a: i if txt[i] == ',' else n])
+                    pushed_a = True
+        if (quote is not None) or (brace is not None):
+            raise SyntaxError()
+        parts = list(map(lambda x: x.strip(), parts))
+        return parts
 
     def do_help(self, arg):
         """To get help on a command, type help <topic>.
@@ -116,6 +222,28 @@ class HBNBCommand(cmd.Cmd):
         setattr(req_instance, args[2], args[3])
         req_instance.save()
 
+    def do_count(self, line):
+        """Prints the number of instances of a class.
+        Usage: count <class name>
+        """
+        args = []
+        try:
+            args = shlex.split(line)
+        except Exception:
+            print('*** Unknown syntax: {}'.format(line))
+            return
+        class_name = args[0] if len(args) >= 1 else None
+        if class_name is None:
+            print("** class name missing **")
+            return
+        if class_name in storage.model_classes.keys():
+            n = 0
+            for obj in storage.all().values():
+                if type(obj) is storage.model_classes[class_name]:
+                    n += 1
+            print(n)
+        else:
+            print("** class doesn't exist **")
 
 def check_classname(args, id=False):
     """Runs checks on args to validate classname entry.
